627b3eba939d62569bd7553710bfd462386c953c
Bohong Huang <bohonghuang@qq.com>
2026-03-12 00:47:15 +0800

    Define specialized parsers for integer types

--- src/type/integer.lisp
+++ src/type/integer.lisp
@@ -8,15 +8,34 @@
 
 (declaim (ftype (function (list) (values integer)) bytes-unsigned-integer/le))
 (defun bytes-unsigned-integer/le (bytes)
-  (loop :for byte :in bytes
-        :for shift :from 0 :by 8
+  (loop :for byte :of-type (unsigned-byte 8) :in bytes
+        :for shift :of-type non-negative-fixnum :from 0 :by 8
         :sum (ash byte shift)))
 
 (declaim (ftype (function (list) (values integer)) bytes-unsigned-integer/be))
 (defun bytes-unsigned-integer/be (bytes)
-  (loop :for byte :in bytes
-        :for shift :downfrom (* 8 (1- (length bytes))) :by 8
+  (loop :for byte :of-type (unsigned-byte 8) :in bytes
+        :for shift :of-type non-negative-fixnum :downfrom (* 8 (1- (length bytes))) :by 8
         :sum (ash byte shift)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defconstant +word-size+ (* (ceiling (log most-positive-fixnum 2) 8) 8))
+  (defun unsigned-byte-parser (n endian)
+    (with-gensyms (bytes)
+      `(for ((,bytes (list . ,(loop :repeat (floor n 8) :collect `(unsigned-byte-8)))))
+         (the (unsigned-byte ,n)
+              ,(if (<= n +word-size+)
+                   (with-gensyms (byte shift)
+                     (ecase endian
+                       (:big `(loop :for ,byte :of-type (unsigned-byte 8) :in ,bytes
+                                    :for ,shift :of-type non-negative-fixnum :downfrom ,(- n 8) :by 8
+                                    :sum (the (unsigned-byte ,n) (ash ,byte ,shift)) :of-type (unsigned-byte ,n)))
+                       (:little `(loop :for ,byte :of-type (unsigned-byte 8) :in ,bytes
+                                       :for ,shift :of-type non-negative-fixnum :from 0 :by 8
+                                       :sum (the (unsigned-byte ,n) (ash ,byte ,shift)) :of-type (unsigned-byte ,n)))))
+                   (ecase endian
+                     (:big `(bytes-unsigned-integer/be ,bytes))
+                     (:little `(bytes-unsigned-integer/le ,bytes)))))))))
 
 (declaim (inline unsigned-signed-integer))
 (defun unsigned-signed-integer (unsigned nbits)
@@ -30,14 +49,32 @@
       `(for ((,unsigned ,(expand-type `(unsigned-byte ,n))))
          (the (signed-byte ,n) (unsigned-signed-integer ,unsigned ,n))))))
 
+(defmacro define-unsigned-integer-parsers ()
+  (loop :with mappings
+        :for n :from 8 :to (max +word-size+ 64) :by 8
+        :nconc (loop :for endian :in '(:little :big)
+                     :for name := (setf (assoc-value mappings (list `(unsigned-byte ,n) endian) :test #'equal)
+                                        (intern (format nil "~A-~D~@[/~A~]" 'unsigned-byte n (when (> n 8) (ecase endian (:little 'le) (:big 'be))))))
+                     :when (> n 8) :collect `(defparser ,name () ,(unsigned-byte-parser n endian)))
+          :into parsers
+        :finally
+           (return
+             (with-gensyms (name type endian)
+               `(progn
+                  (defun integer-type-parser (,type &optional (,endian :little))
+                    (if-let ((,name (assoc-value ',mappings (list ,type ,endian) :test #'equal)))
+                      (list ,name)
+                      (progn
+                        (assert (eq (first ,type) 'unsigned-byte))
+                        (unsigned-byte-parser (second ,type) ,endian))))
+                  ,@parsers)))))
+
+(define-unsigned-integer-parsers)
+
 (defmethod expand-type-expr ((name (eql 'unsigned-byte)) &rest args)
   (destructuring-bind (n) args
     (prog1 (if (and (integerp *offset*) (zerop (mod n 8)))
-               (with-gensyms (bytes)
-                 `(for ((,bytes (list . ,(loop :repeat (floor n 8) :collect `(unsigned-byte-8)))))
-                    (the (unsigned-byte ,n) ,(ecase *endian*
-                                               (:big `(bytes-unsigned-integer/be ,bytes))
-                                               (:little `(bytes-unsigned-integer/le ,bytes))))))
+               (integer-type-parser (cons name args) *endian*)
                (let ((bit-offset (/ *offset* 1/8)))
                  `(constantly (the (unsigned-byte ,n) (ldb (byte ,n ,bit-offset) byte)))))
       (incf *offset* (/ n 8)))))
