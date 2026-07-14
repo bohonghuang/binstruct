8d325b1654b6f6492a8f3298bd99f3f8c5cdade8
Bohong Huang <bohonghuang@qq.com>
2026-07-14 20:08:37 +0800

    Improve naming

--- src/package.lisp
+++ src/package.lisp
@@ -1,8 +1,5 @@
 (defpackage binstruct
   (:use #:cl #:alexandria #:parsonic)
-  (:import-from #:float-features
-   #:bits-single-float #:bits-double-float
-   #:single-float-bits #:double-float-bits)
   (:import-from #:buffered-streams #:copy-cons)
   (:export
    #:defbinenum
