30bde864ca1c82cd635b7693903f5f2b16d76130
Bohong Huang <bohonghuang@qq.com>
2026-07-15 22:46:20 +0800

    Allow specifying `map`'s Lisp type via `the` on the reader function

--- src/type/map/reader.lisp
+++ src/type/map/reader.lisp
@@ -13,5 +13,23 @@
          (funcall ,reader ,value)))))
 
 (defmethod lisp-type-expr ((name (eql 'map)) &rest args)
-  (declare (ignore args))
-  t)
+  (destructuring-bind (type &optional (reader '#'identity) (writer '#'identity)) args
+    (declare (ignore type writer))
+    (when (consp reader)
+      (destructuring-case reader
+        ((the type function)
+         (declare (ignore function))
+         (when (consp type)
+           (destructuring-case type
+             ((function (&rest args) &optional (type 'null))
+              (declare (ignore args))
+              (if (consp type)
+                  (destructuring-case type
+                    ((values &optional (type 'null) &rest args)
+                     (declare (ignore args))
+                     (return-from lisp-type-expr type))
+                    ((t &rest args)
+                     (declare (ignore args))
+                     (return-from lisp-type-expr type)))
+                  (return-from lisp-type-expr type))))))))
+    t))
