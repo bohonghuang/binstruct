af351527c5cb293c2f3bd88749f5751598de8724
Bohong Huang <bohonghuang@qq.com>
2026-07-24 14:29:07 +0800

    Pad `simple-base-string` to declared length when writing

--- src/type/string/writer.lisp
+++ src/type/string/writer.lisp
@@ -1,13 +1,18 @@
 (in-package #:binstruct)
 
-(declaim (ftype (function (emitter-output simple-base-string)) emit-simple-base-string))
-(defun emit-simple-base-string (output string)
-  (loop :for char :of-type base-char :across string
-        :do (emitter-output-byte output (char-code char))))
+(declaim (ftype (function (emitter-output simple-base-string &optional non-negative-fixnum)) emit-simple-base-string))
+(defun emit-simple-base-string (output string &optional (length 0))
+  (loop :for written :of-type non-negative-fixnum :from 0
+        :for char :of-type base-char :across string
+        :do (emitter-output-byte output (char-code char))
+        :finally (loop :repeat (- length written) :do (emitter-output-byte output #x00))))
 
 (defmethod expand-writer-type-expr ((name (eql 'simple-base-string)) &rest args)
   (destructuring-bind (&optional (length '*)) args
-    `(progn
-       (emit-simple-base-string ,*output* ,*value*)
-       ,(case length
-          (* `(emitter-output-byte ,*output* #x00))))))
+    (once-only (*value*)
+      `(emit-simple-base-string
+        ,*output*
+        ,*value*
+        ,(case length
+           (* `(1+ (length ,*value*)))
+           (t length))))))
