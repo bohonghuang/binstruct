616633165518237fd9d0fbe8a730291826a866c9
Bohong Huang <bohonghuang@qq.com>
2026-09-03 22:00:46 +0800

    Fix illegal code generated for arrays with unknown default elements

--- src/type/array/reader.lisp
+++ src/type/array/reader.lisp
@@ -48,15 +48,18 @@
   (finish-reader-partial-byte)
   (destructuring-bind (element-type (dimension)) array-type-args
     (with-gensyms (array index length count)
-      (let ((name (car (first *slots*))))
+      (let ((name (slot-name))
+            (initial-element (type-default-value element-type)))
         (multiple-value-bind (bindings elements)
-            (let ((*slots* (with-gensyms (slot) `((,slot type-default-value :type ,element-type))))
-                  (*place* (place-lambda (value) `(when (< ,index (length ,array)) (setf (aref ,array ,index) ,value))))
-                  (*offset* 0))
-              (let* ((slots (loop :for i :below (denominator (type-offset element-type))
-                                  :collect `(,(with-gensyms (element) element) nil :type (capture-index ,index ,i ,element-type))))
-                     (results (mapcar #'first slots)))
-                (values (slots-parser-bindings slots) results (assert (integerp *offset*)))))
+            (let ((*offset* 0))
+              (if (eq initial-element 'type-default-value)
+                  (let ((*place* (place-null)))
+                    (with-gensyms (slot)
+                      (values (slots-parser-bindings `((,slot type-default-value :type ,element-type))) (list slot))))
+                  (loop :with *place* := (place-lambda (value) `(when (< ,index (length ,array)) (setf (aref ,array ,index) ,value)))
+                        :for i :below (denominator (type-offset element-type))
+                        :collect `(,(with-gensyms (element) element) nil :type (capture-index ,index ,i ,element-type)) :into slots
+                        :finally (return (values (slots-parser-bindings slots) (mapcar #'first slots) (assert (integerp *offset*)))))))
           (if (equal element-type '(unsigned-byte 8))
               (if name
                   `(sequence/fixed-length (unsigned-byte-8) ,dimension ',(lisp-type (cons array-type array-type-args)))
@@ -67,32 +70,31 @@
                    (declare (type non-negative-fixnum ,count) (ignorable ,count))
                    ,(let ((count-max (case dimension (* most-positive-fixnum) (t count))))
                       (if name
-                          (let ((initial-element (type-default-value element-type)))
-                            (if (eq initial-element 'type-default-value)
-                                (let ((parser `(sequence/fixed-length
-                                                (let* ,bindings (list . ,(mapcar (curry #'list 'constantly) elements)))
-                                                ,count '(,array-type ,(lisp-type element-type) (*)) #'flatten-list)))
-                                  (case dimension
-                                    (* (setf (car parser) 'sequence/until-failure) (delete count parser))
-                                    (t parser)))
-                                `(let ((,array (constantly (make-array ,length :element-type ',(lisp-type element-type)
-                                                                               :initial-element ,initial-element
-                                                                               ,@(case dimension (* `(:fill-pointer 0 :adjustable t))))))
-                                       (,index (constantly 0)))
-                                   (declare (type (,(case dimension (* 'array) (t 'simple-array)) ,(lisp-type element-type) (*)) ,array)
-                                            (type non-negative-fixnum ,index))
-                                   (for ((nil (rep (let* ,bindings
-                                                     ((lambda ()
-                                                        ,@(loop :for element :in elements
-                                                                :for i :from 0
-                                                                :collect `(,@(if (or (zerop i) (eq dimension '*)) '(progn) `(when (< ,index ,length)))
-                                                                           ,(case dimension
-                                                                              (* `(vector-push-extend ,element ,array))
-                                                                              (t `(setf (aref ,array ,index) ,element)))
-                                                                           (incf ,index)))
-                                                        (parser (constantly nil)))))
-                                                   ,count ,count-max)))
-                                     (setf ,array (coerce ,array '(,array-type ,(lisp-type element-type) (*))))))))
+                          (if (eq initial-element 'type-default-value)
+                              (let ((parser `(sequence/fixed-length
+                                              (let* ,bindings (list . ,(mapcar (curry #'list 'constantly) elements)))
+                                              ,count '(,array-type ,(lisp-type element-type) (*)) #'flatten-list)))
+                                (case dimension
+                                  (* (setf (car parser) 'sequence/until-failure) (delete count parser))
+                                  (t parser)))
+                              `(let ((,array (constantly (make-array ,length :element-type ',(lisp-type element-type)
+                                                                             :initial-element ,initial-element
+                                                                             ,@(case dimension (* `(:fill-pointer 0 :adjustable t))))))
+                                     (,index (constantly 0)))
+                                 (declare (type (,(case dimension (* 'array) (t 'simple-array)) ,(lisp-type element-type) (*)) ,array)
+                                          (type non-negative-fixnum ,index))
+                                 (for ((nil (rep (let* ,bindings
+                                                   ((lambda ()
+                                                      ,@(loop :for element :in elements
+                                                              :for i :from 0
+                                                              :collect `(,@(if (or (zerop i) (eq dimension '*)) '(progn) `(when (< ,index ,length)))
+                                                                         ,(case dimension
+                                                                            (* `(vector-push-extend ,element ,array))
+                                                                            (t `(setf (aref ,array ,index) ,element)))
+                                                                         (incf ,index)))
+                                                      (parser (constantly nil)))))
+                                                 ,count ,count-max)))
+                                   (setf ,array (coerce ,array '(,array-type ,(lisp-type element-type) (*)))))))
                           `(progn (rep (let* ,bindings (constantly (progn . ,elements))) ,count ,count-max) (constantly nil))))))))))))
 
 (defmethod parsonic::expand-expr ((name (eql 'simple-array)) &rest args)
